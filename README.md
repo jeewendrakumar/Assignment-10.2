1a1a2c8e417d55981b5f44200e1e98e938c2a8ca
jeewendra kumar <jeewendrakumar@gmail.com>
2017-05-29 18:57:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Assignment-10.2
+# AcadGild - Web application development with React and Relay
+
+## Session 5
+#### To Build
+Run: `npm install`
+
+### To start:
+Run: `npm start`
---
+++ app/js/components/Books.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Book from "./Book";
+
+export default class Books extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            activeId:""
+        };
+    }
+    setStatus = (id) => {
+        this.setState({activeId:id});
+    }
+    render() {
+        const {books, deleteBook} = this.props; //Getting data using props.
+
+        return (
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>BOOK</th>
+                            <th>DELETE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            books.map((book) => {
+                                return (<Book key={book.id} book={book} deleteBook={deleteBook} active={this.state.activeId === book.id} setStatus={this.setStatus}/>);
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
